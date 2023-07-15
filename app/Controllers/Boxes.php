dffa317feb85b6fd55fe5cfa3ecbb72a7b3c4511
nort-jamz <james_at@hotmail.com>
2023-07-15 20:35:54 +0200

    good

---
+++ app/Controllers/Boxes.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\BoxesModels;
+
+class Auth extends BaseController
+{
+
+    public function __construct()
+    {
+        $this->BoxesModels = new BoxesModels();
+    }
+
+    public function index()
+    {
+        echo view("layout/header");
+        echo view("layout/aside");
+        echo view("layout/footer");
+    }
+
+}
